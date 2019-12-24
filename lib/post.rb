2ee2a2b3a6cbf1a6167623f4a205e45d009f4815
Yamin Ghowrwal <yamin.gh@gmail.com>
2019-12-24 23:19:23 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,18 @@
+class Author
+
+  attr_accessor :name
+
+  def initialize
+    @name = name
+  end
+
+end
+
+class Post
+  attr_accessor  :title, :author
+
+  def initialize
+    @title = title
+    @author = author
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Artist
+  attr_accessor :name
+
+  def initialize
+    @name = name
+  end
+end
+
+ class Song
+   attr_accessor  :title, :artist
+
+  def initialize
+    @title = title
+    @artist = artist
+  end
+
+end
